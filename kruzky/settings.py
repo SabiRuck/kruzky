ee6c65c57be263ec8e6d6e2b040cd187d6b30b74
Sabi Ruckova <sabinka.ruckova@gmail.com>
2026-04-01 08:51:01 +0200

    cscsc

--- kruzky/settings.py
+++ kruzky/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
